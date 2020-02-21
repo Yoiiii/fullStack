33151c25c9fe64602af9a6b8c4926607851ebf0e
邓凌霄 <593409855@qq.com>
2020-02-21 15:48:58 +0800

    'text'

--- admin/src/views/Main.vue
+++ admin/src/views/Main.vue
@@ -2,10 +2,10 @@
 <template>
   <el-container style="height: 100vh;">
     <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-      <el-menu router unique-opened :default-active="$route.path">
+      <el-menu router  :default-active="$route.path">
         <el-submenu index="1">
           <template slot="title">
-            <i class="el-icon-message"></i>内容管理
+            <i class="el-icon-edit-outline"></i>内容管理
           </template>
           <el-menu-item-group>
             <template slot="title">物品</template>
@@ -30,7 +30,7 @@
         </el-submenu>
         <el-submenu index="2">
           <template slot="title">
-            <i class="el-icon-message"></i>运营管理
+            <i class="el-icon-date"></i>运营管理
           </template>
           <el-menu-item-group>
             <template slot="title">广告位</template>
@@ -40,7 +40,7 @@
         </el-submenu>
         <el-submenu index="3">
           <template slot="title">
-            <i class="el-icon-message"></i>系统设置
+            <i class="el-icon-setting"></i>系统设置
           </template>
           <el-menu-item-group>
             <template slot="title">管理员</template>
