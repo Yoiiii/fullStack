a1726909f95a227a3b4c9245b9d923f0a5860645
Yoiiii <majestyling@hotmail.com>
2020-02-24 22:22:15 +0800

    P3.7

---
+++ web/src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+  <swiper :options="swiperOption" ref="mySwiper" @someSwiperEvent="callback">
+    <!-- slides -->
+    <swiper-slide>
+      <img class="w-100" src="../assets/images/black.png">
+    </swiper-slide>
+    <swiper-slide>
+      <img  class="w-100" src="../assets/images/black.png">
+    </swiper-slide>
+        <swiper-slide>
+      <img  class="w-100" src="../assets/images/black.png">
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination pagination-home text-right"  slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      swiperOption: {
+          pagination: {
+            el: '.pagination-home'
+          }
+        },
+    }
+  }
+}
+</script>
+<style lang="scss">
+@import '../assets/scss/variables';
+.pagination-home{
+  .swiper-pagination-bullet{
+    background: #fff;
+  }
+  &.swiper-pagination-bullet-active{
+    background: map-get($colors,'info');
+  }
+}
+  
+</style>
---
+++ web/src/views/Main.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="topbar bg-black py-2 text-white px-3 d-flex ai-center">
+      <img class="bg-white" src="../assets/logo.png" height="30" />
+      <div class="px-2 flex-grow-1">
+        <div class="text-white">Vue项目</div>
+        <div class="text-gary-1 fs-xxs">全栈</div>
+      </div>
+      <button type="button" class="btn bg-primary">立即进入</button>
+    </div>
+    <div class="bg-primary pt-3 pb-2">
+      <div class="nav d-flex text-white jc-around pb-1">
+        <div class="nav-item active">
+          <router-link class="nav-link" tag="div" to="/">首页</router-link>
+        </div>
+        <div class="nav-item">
+          <router-link class="nav-link" tag="div" to="/">攻略中心</router-link>
+        </div>
+        <div class="nav-item">
+          <router-link class="nav-link" tag="div" to="/">赛事中心</router-link>
+        </div>
+      </div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
