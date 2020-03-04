e82623fd83132baddf058ca8f471abb85fce16fb
Yoiiii <majestyling@hotmail.com>
2020-03-04 22:57:07 +0800

    finish P14 3.14

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -31,17 +31,23 @@
     <m-list-card icon="cc-menu-circle" title="新闻资讯" :categories="newsCats">
       <template #items="{category}">
         <div class="py-2 fs-lg d-flex" v-for="(news,i) in category.newsList" :key ="i">
-          <span class="text-info">[{{news.categoryName}}]</span>
+          <span class="text-info" style="white-space: nowrap">[{{news.categoryName}}]</span>
           <span class="px-2">|</span>
-          <span class="flex-1">{{news.title}}</span>
-          <span>{{news.createdAt}}</span>
+          <span class="flex-grow-1 text-dark-1 text-ellipsis pr-2">{{news.title}}</span>
+          <span class="text-grey-1 fs-sm">{{news.createdAt | date}}</span>
         </div>
       </template>
     </m-list-card>
   </div>
 </template>
 <script>
+import dayjs from 'dayjs'
 export default {
+  filters:{
+    date(val){  
+      return dayjs(val).format('MM/DD')
+    }
+  },
   data() {
     return {
       swiperOption: {
